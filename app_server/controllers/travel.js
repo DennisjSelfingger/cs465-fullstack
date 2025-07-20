5a51145b46af9bf4dbbe82789874b7e47a550eac
Denns Selfinger <Dself1976@gmail.com>
2025-07-20 19:37:14 -0400

    Completed Module 3 - Dynamic HBS rendering with JSON data

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,17 @@
 /* GET travel page */
+var fs = require('fs');
+
 const travel = (req, res, next) => {
-  res.render('travel', { title: 'Travlr Getaways' });
+  // Read the trips.json file
+  var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+  
+  // Pass trips data to the template
+  res.render('travel', { 
+    title: 'Travlr Getaways',
+    trips: trips 
+  });
 };
+
 module.exports = {
   travel
 };
